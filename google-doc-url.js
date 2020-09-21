30ec761f3571f2ea1137d6471208db1b5163718c
Tim McGrath <tim@wanderfeast.com>
2020-09-21 12:44:45 -0700

    Fixing URL
    Fixing URL

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1WDrDKACpgcddBBqXCiMbibMkiX2ONvfFw2E8e99iePk/edit#gid=1033046795';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1WDrDKACpgcddBBqXCiMbibMkiX2ONvfFw2E8e99iePk/';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBgIfO3GG-ThELVP2sS66Hz953JZyWoXP0';
